162f516ec0d409fd40125efd7f08b2df23bdbe88
filfab <284025@student.pwr.edu.pl>
2025-05-16 00:24:02 +0200

    2.1/3

--- src/App.java
+++ src/App.java
@@ -5,45 +5,33 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.util.converter.NumberStringConverter;
+
 
 public class App extends Application {
     DoubleProperty mouseX = new SimpleDoubleProperty();
     DoubleProperty mouseY = new SimpleDoubleProperty();
     IntegerProperty selectedButton = new SimpleIntegerProperty(-1);
     IntegerProperty selectedShape = new SimpleIntegerProperty(-1);
-    BooleanProperty isInfoStageOpen = new SimpleBooleanProperty(false);
-    BooleanProperty isHelpStageOpen = new SimpleBooleanProperty(false);
+    BooleanProperty[] openWindowFlags;
     Stage primaryStage;
-    Border test;
 
 
-    /**
-     * test
-     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
     @Override
     public void start(Stage _primaryStage) {
-        test = new Border(new BorderStroke(Color.ALICEBLUE, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.FULL));
         primaryStage = _primaryStage;
         Scene scene = new Scene(createContent(), 1920, 1080);
 
@@ -52,21 +40,9 @@ public class App extends Application {
         primaryStage.setMinHeight(400);
         primaryStage.setScene(scene);
         primaryStage.show();
-        mouseX.setValue(100.12);
     }
     
-    /**
-     * Main function
-     * @param args
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
 
-    /**
-     * Returns the content of the main scene
-     * @return
-     */
     private Region createContent() {
         BorderPane root = new BorderPane();
 
@@ -75,51 +51,13 @@ public class App extends Application {
         return root;
     }
 
-    /**
-     * Returns the content of center pane of main scene
-     * @return
-     */
+
     private Region createCenter() {
-        AnchorPane canvas = new AnchorPane();
+        AnchorPane canvas = new Utils.Canvas(mouseX, mouseY, selectedButton);
         canvas.setMaxSize(1920, 1080);
         canvas.setMinSize(1920, 1080);
         canvas.setPrefSize(1920, 1080);
         canvas.setStyle("-fx-background-color: white");
-
-        canvas.setOnMouseMoved(event -> {
-            mouseX.set(event.getX());
-            mouseY.set(event.getY());
-        });
-
-        canvas.setOnMouseDragged(event -> {
-            mouseX.set(event.getX());
-            mouseY.set(event.getY());
-        });
-
-        canvas.setOnMouseExited(event -> {
-            mouseX.set(-1);
-            mouseY.set(-1);            
-        });
-
-        canvas.setOnMousePressed(event -> {
-            switch (event.getButton()) {
-                case PRIMARY:
-                    switch (selectedButton.get()) {
-                        case 0:
-                            canvas.getChildren().add(new Circle(mouseX.get(), mouseY.get(), 5));
-                            break;
-                        case 1:
-                            canvas.getChildren().add(new Rectangle(mouseX.get(), mouseY.get(), 10 ,10));
-                            break;
-                        default:
-                            break;
-                    }
-                break;
-            
-                default: break;
-            }
-            
-        });
 
         ScrollPane center = new ScrollPane(canvas);
         center.setFitToHeight(true);
@@ -128,94 +66,38 @@ public class App extends Application {
         return center;
     }
 
-
-    /**
-     * Returns the content of bottom pane of main scene
-     * @return
-     */
+    
     private Region createBottom() {
-        Label mX = new Label();
-        Label mY = new Label();
-        mX.textProperty().bindBidirectional(mouseX, new NumberStringConverter());
-        mY.textProperty().bindBidirectional(mouseY, new NumberStringConverter());
-        HBox cords = new HBox(mX, new Label(" : "), mY);
-        cords.setAlignment(Pos.CENTER);
-        cords.setMinWidth(50);
+        HBox cords = new Utils.Cords(mouseX, mouseY);
 
-        HBox menu = new HBox(
-            10,
-            createMenuButton("circle", 0),
-            createMenuButton("rect", 1),
-            createMenuButton("poly", 2),
-            createMenuButton("edit", 3)
-            );
+        String[] labels = new String[]{"circle", "rect", "poly" ,"edit"};
+        Button[] buttons = new Button[labels.length];
+        for (int i=0; i<labels.length; i++) {
+            buttons[i] = new Utils.MenuButton(labels[i], i, selectedButton);
+        }
+        HBox menu = new HBox(10, buttons);
         menu.setAlignment(Pos.CENTER);
 
-        HBox buttons = new HBox(10, createHelpButton(), createInfoButton());
-        buttons.setAlignment(Pos.CENTER);
+
+        labels = new String[]{"info", "help"};
+        openWindowFlags = new BooleanProperty[labels.length];
+        buttons = new Button[labels.length];
+        for (int i=0; i<labels.length; i++) {
+            openWindowFlags[i] = new SimpleBooleanProperty(false);
+            buttons[i] = new Utils.InfoButton(labels[i], primaryStage, openWindowFlags[i]);
+        }
+        HBox info = new HBox(10, buttons);
+        info.setAlignment(Pos.CENTER);
+        info.setMinWidth(80);
+
         
         BorderPane bottom = new BorderPane();
         bottom.setMinHeight(40);
         bottom.setStyle("-fx-background-color: grey; -fx-padding: 0 10 0 10;");
-        
         bottom.setLeft(cords);
         bottom.setCenter(menu);
-        bottom.setRight(buttons);
+        bottom.setRight(info);
         return bottom;
-    }
-
-
-    private Button createHelpButton() {
-        Button button = new Button("HELP");
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                if (!isHelpStageOpen.get()) {
-                    Stage helpStage = createChildStage(primaryStage, isHelpStageOpen);
-                    helpStage.setScene(new Scene(new HBox(4, new Label("help window"))));
-                    helpStage.show();
-                    isHelpStageOpen.set(true);
-                }
-            }
-        });
-        return button;
-    }
-
-    private Button createInfoButton() {
-        Button button = new Button("INFO");
-        String text = "info window";
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                if (!isInfoStageOpen.get()) {
-                    Stage infoStage = createChildStage(primaryStage, isInfoStageOpen);
-                    infoStage.setScene(new Scene(new HBox(4, new Label(text))));
-                    infoStage.show();
-                    isInfoStageOpen.set(true);
-                }
-            }
-        });
-        return button;
-    }
-
-
-    private Stage createChildStage(Stage owner, BooleanProperty flagPtr) {
-        Stage stage = new Stage();
-        stage.initOwner(owner);
-        stage.setOnHiding(e -> flagPtr.set(false));
-
-        return stage;
-    }
-
-
-    private Button createMenuButton(String text, int id) {
-        Button button = new Button(text);
-        button.setOnAction(event -> {
-           selectedButton.set(id); 
-        });
-        button.setBorder(selectedButton.isEqualTo(id).get() ? test : Border.EMPTY);
-
-        return button;
     }
 
 }
--- src/Utils.java
+++ src/Utils.java
@@ -1,4 +1,205 @@
+import java.util.ArrayList;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
+
 public class Utils {
 
-    
+    public static Double distance(Double x0, Double y0, Double x1, Double y1) {
+        return Math.sqrt(Math.pow(x0 - x1, 2) + Math.pow(y0 - y1, 2));
+    }
+
+
+    public static class MenuButton extends Button {  
+        MenuButton(String text, int id, IntegerProperty selectedButton, ArrayList<Double> points) {
+            super(text);
+
+            this.setOnAction(event -> {
+                selectedButton.set(id);
+                points.clear();
+                
+            });
+        }
+    }
+
+
+    public static class InfoButton extends Button {
+        InfoButton(String text, Stage primaryStage, BooleanProperty isOpen) {
+            super(text);
+
+            this.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    if (!isOpen.get()) {
+                        Stage infoStage = new ChildStage(text, primaryStage, isOpen);
+                        infoStage.show();
+                        isOpen.set(true);
+                    }
+                }
+            });
+        }
+    }
+
+
+    public static class ChildStage extends Stage {
+        ChildStage(String text, Stage owner, BooleanProperty isOpen) {
+            super();
+
+            Label content = new Label(text);
+            content.setAlignment(Pos.CENTER);
+
+            this.setScene(new Scene(content, 400, 400));
+            this.setResizable(false);
+            this.initOwner(owner);
+            this.setOnCloseRequest(event -> {
+                isOpen.set(false);
+            });
+        }
+    }
+
+
+    public static class Canvas extends AnchorPane {
+        private Shape shapePreview = null;
+
+
+        Canvas(DoubleProperty mouseX, DoubleProperty mouseY, IntegerProperty selectedButton, ArrayList<Double> points) {
+            super();
+
+            EventHandler<MouseEvent> mousePositionUpdate = new EventHandler<MouseEvent>() {
+                public void handle(MouseEvent event) {
+                    mouseX.set(event.getX());
+                    mouseY.set(event.getY());
+
+                    if (shapePreview != null) {
+                        switch (selectedButton.get()) {
+                            case 0:
+                                ((Circle) shapePreview).setRadius(distance(((Circle) shapePreview).getCenterX(), ((Circle) shapePreview).getCenterY(), mouseX.get(), mouseY.get()));
+                                break;
+
+                            case 1:
+                                ((Rectangle) shapePreview).setX(Math.min(((Rectangle) shapePreview).getX(), event.getX()));
+                                ((Rectangle) shapePreview).setY(Math.min(((Rectangle) shapePreview).getY(), event.getY()));
+                                ((Rectangle) shapePreview).setWidth(event.getX() - ((Rectangle) shapePreview).getX());
+                                ((Rectangle) shapePreview).setHeight(event.getY() - ((Rectangle) shapePreview).getY());
+                                break;
+                            
+                            case 2:
+                                ((Polygon) shapePreview).getPoints().set(((Polygon) shapePreview).getPoints().size()-2, event.getX());
+                                ((Polygon) shapePreview).getPoints().set(((Polygon) shapePreview).getPoints().size()-1, event.getY());
+                        
+                            default:
+                                break;
+                        }
+                    }
+                }
+            };            
+            this.setOnMouseMoved(mousePositionUpdate);
+            this.setOnMouseDragged(mousePositionUpdate);
+
+            this.setOnMouseExited(event -> {
+                mouseX.set(-1);
+                mouseY.set(-1);
+            });
+
+            this.setOnMousePressed(event -> {
+                switch (event.getButton()) {
+                    case PRIMARY:
+                        switch (selectedButton.get()) {
+                            case 0:
+                                if (shapePreview == null) {
+                                    shapePreview = new Circle(event.getX(), event.getY(), 0);
+                                    this.getChildren().add(shapePreview);
+                                } else {
+                                    shapePreview = null;
+                                }
+                                break;
+                    
+                            case 1:
+                                if (shapePreview == null) {
+                                    shapePreview = new Rectangle(event.getX(), event.getY(), event.getX(), event.getY());
+                                    this.getChildren().add(shapePreview);
+                                } else {
+                                    shapePreview = null;
+                                }
+                                break;
+                        
+                            case 2:
+                                if (shapePreview == null) {
+                                    shapePreview = new Polygon(event.getX(), event.getY(), event.getX(), event.getY());
+                                    this.getChildren().add(shapePreview);
+                                } else if (distance(event.getX(), event.getY(), ((Polygon) shapePreview).getPoints().get(0), ((Polygon) shapePreview).getPoints().get(1)) <= 10) {
+                                    shapePreview = null;
+                                } else {
+                                    ((Polygon) shapePreview).getPoints().addAll(event.getX(), event.getY());
+                                }
+                                break;
+
+                            default:
+                                break;
+                        }
+                    break;
+
+                    case SECONDARY:
+                        switch (selectedButton.get()) {
+                            case 0,1,2:
+                                if (shapePreview != null) {
+                                    this.getChildren().removeLast();
+                                    shapePreview = null;
+                                }
+                                break;
+                        
+                            default:
+                                break;
+                        }
+                
+                    default: break;
+                }
+                
+            });
+        }
+    }
+
+
+    public static class Cords extends HBox {
+        Cords(DoubleProperty mouseX, DoubleProperty mouseY) {
+            super();
+
+            Label mX = new Label();
+            Label mY = new Label();
+            mX.textProperty().bindBidirectional(mouseX, new NumberStringConverter());
+            mY.textProperty().bindBidirectional(mouseY, new NumberStringConverter());
+
+            this.getChildren().addAll(mX, new Label(" : "), mY);
+            this.setAlignment(Pos.CENTER);
+            this.setMinWidth(80);
+        }
+    }
+
+
+    public static class PolygonStartPoint extends Button{
+        PolygonStartPoint() {
+            super();
+
+            this.setOnAction(event -> {
+
+            });
+        }
+    }
+
 }
